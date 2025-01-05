e6db980090d943282980ef8b6922bb0505be709a
shubha113 <85664043+shubha113@users.noreply.github.com>
2025-01-05 16:14:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: 'http://localhost:5173', credentials: true, methods:["GET", "POST","PUT","DELETE"],}));
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true,
